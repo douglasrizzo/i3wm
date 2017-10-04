9e9deecf90d9b3e13b4a33a25b77e5259de997cb
Tiago M. De Rizzo <tmrizzo@none.com>
2017-10-03 22:46:47 -0300

    Added screen layout bindings and scripts

---
+++ screenlayout/displays_home1.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --primary --mode 1600x900 --pos 0x0 --rotate normal --output DP2-1 --off --output DP2-2 --off --output VIRTUAL1 --off --output DP1 --off --output DP2 --off --output HDMI2 --off --output HDMI1 --off --output VGA1 --off
