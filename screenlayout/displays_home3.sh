b2087e615f2702ecdf4f753db513667004edad77
Tiago M. De Rizzo <tmrizzo@none.com>
2017-11-11 13:37:20 -0200

    Added: display xrandr script for left monitor. Changed: display modes.

--- screenlayout/displays_home3.sh
+++ screenlayout/displays_home3.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1600x900 --pos 1440x0 --rotate normal --output DP1 --off --output DP2-1 --mode 1360x768 --pos 3040x0 --rotate normal --output DP2-2 --mode 1440x900 --pos 0x0 --rotate normal --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output VGA1 --off --output DP2 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1600x900 --pos 1680x0 --rotate normal --output DP1 --off --output DP2-1 --mode 1360x768 --pos 3280x0 --rotate normal --output DP2-2 --mode 1680x1050 --pos 0x0 --rotate normal --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output VGA1 --off --output DP2 --off
---
+++ screenlayout/displays_home4.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1600x900 --pos 1680x0 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --mode 1680x1050 --pos 0x0 --rotate normal --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output VGA1 --off --output DP2 --off
